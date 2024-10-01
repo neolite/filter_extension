e4f1a1a3bb09debb73ebe3dc73210e5e0eef4b9e
rafkatg <103734035+rafkatg@users.noreply.github.com>
2024-10-01 11:11:11 +0300

    feat: add a popup

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Popup: React.FC = () => {
+	return (
+		<div>
+			<h1>Filter URL Extension</h1>
+			<p>This is a basic popup.</p>
+		</div>
+	);
+};
+
+export default Popup;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('node:path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    popup: './src/popup/index.tsx',
+    background: './src/background/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/popup.html',
+      filename: 'popup.html',
+      chunks: ['popup']
+    })
+  ]
+};
+
